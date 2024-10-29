0c680983e6759cfa5641b29187320746a49bd494
Denis <draghici.denis3@gmail.com>
2024-10-29 13:37:17 +0200

    update 3rd day

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -20,7 +20,7 @@
           </div>
           <p class="text-2xl font-semibold text-gray-900 dark:text-gray-100">{{ stats.totalQuizzes }}</p>
           <p class="text-sm text-gray-500 dark:text-gray-400 mt-2">
-            {{ stats.publishedQuizzes }} publicate
+            {{ stats.publishedQuizzes }} publicate.
           </p>
         </div>
 
